77efa75c5f6c900b23088ef6c5524d240a860eab
Ketan <ketantoo@gmail.com>
2021-01-02 19:16:34 +0530

    finished codebase

--- const.js
+++ const.js
@@ -1,2 +1,2 @@
-export const URL = 'https://api.themoviedb.org/3/';
-export const API_KEY = 'b738a8ca0ae623575444d10ad754948c';
+export const URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "YOUR API";
